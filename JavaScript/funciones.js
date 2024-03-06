06723bf77637a70e9a163fb36e0b04a167933e9d
system-fault <thpuppetmaster@gmail.com>
2024-03-06 01:04:00 +0100

    grid commit

--- JavaScript/funciones.js
+++ JavaScript/funciones.js
@@ -5,10 +5,10 @@
 
 function cambiarImagen() {
     let imagen = document.getElementById("miboton");
-    // Obtenemos la ruta de la imagen actual
+    //? Obtenemos la ruta de la imagen actual
     let rutaActual = imagen.src;
     
-    // Verificamos la ruta actual y cambiamos a la imagen correspondiente
+    //? Verificamos la ruta actual y cambiamos a la imagen correspondiente
     if (rutaActual.includes("boton1.jpg")) {
         imagen.src = "./imagenes/boton2.jpg"; // Cambiar a la segunda imagen
     } else {
@@ -17,17 +17,17 @@ function cambiarImagen() {
 }
 
 function generarTemperatura() {
-    // Generar un número aleatorio entre -10 y 45
+    //? Generar un número aleatorio entre -10 y 45
     let temperatura = Math.floor(Math.random() * (45 - (-10) + 1)) + (-10);
     
-    // Actualizar el texto de la temperatura en el HTML
+    //? Actualizar el texto de la temperatura en el HTML
     document.getElementById("temperatura").innerText = temperatura + " ºC";
 }
 
-// Llamar a la función generarTemperatura inicialmente
+//? Llamar a la función generarTemperatura inicialmente
 generarTemperatura();
 
-// Actualizar la temperatura cada 10 segundos
+//? Actualizar la temperatura cada 10 segundos
 setInterval(generarTemperatura, 5000);
 
 
