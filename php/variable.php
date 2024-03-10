5c4474e93eea6f69791959dbb4988e0024a8ca99
system-fault <thpuppetmaster@gmail.com>
2024-03-10 23:17:22 +0100

    commit >> cambio rama

--- php/variable.php
+++ php/variable.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="es">
 
 <head>
@@ -8,26 +8,30 @@
 </head>
 
 <body>
-    <h1>AQUÍ SE VE EL ESTADO DEL LED.</h1>
+    <h1>AQUÍ SE VE EL ESTADO DEL LED.</h1> -->
 
     <?php
+
+    
     // Verificar si el índice 'dato' está definido en $_POST
     if (isset($_POST['dato'])) {
         // Obtener el estado del LED
         $estado = $_POST['dato'];
         // Establecer una cookie para almacenar el estado del LED
         setcookie("estado_led", $estado, time() + (86400 * 30), "/"); // La cookie expira en 30 días
+        echo $estado;
 
         // Mostrar el estado del LED en la página
-        echo "<p>El estado del LED es: $estado</p>";
+        // echo "<p>El estado del LED es: $estado</p>";
     } else {
         
     // Si no se ha enviado un nuevo estado del LED, recuperar el estado de la cookie (si existe)
     $estado = isset($_COOKIE['estado_led']) ? $_COOKIE['estado_led'] : "No hay datos anteriores";
-        echo "<p>El estado del led no ha cambiado sigue siendo: $estado .</p>";
+        echo $estado;
+        // echo "<p>El estado del led no ha cambiado sigue siendo: $estado .</p>";
     }
     ?>
     
-</body>
+<!-- </body>
 
-</html>
+</html> -->
