c6fde15c0946612b2de87bd2107dafc5897c3733
system-fault <thpuppetmaster@gmail.com>
2024-03-11 14:20:54 +0100

    commit dato independiente de session http ok

---
+++ debugg/test_shmop.php
@@ -0,0 +1,8 @@
+<?php
+if (extension_loaded('shmop')){
+    echo "La extension shmop esta habilitada";
+}else {
+    echo "La extension shmop NO esta cargada";
+}
+
+?>
--- php/variable.php
+++ php/variable.php
@@ -11,22 +11,25 @@
     <h1>AQUÍ SE VE EL ESTADO DEL LED.</h1>
 
     <?php
-    // Tamano del segmento de memoria compartida en (en Bytes)
+    // tamano del segmento de memoria compartida
     $size_mem = 1024;
-    // crea un segmento de memoria compartida
+    // Crea el segmento de memoria compartida
     $shmid = shmop_open(ftok(__FILE__,'t'),"c",0644,$size_mem);
-
-    
 
     // Verificar si el índice 'dato' está definido en $_POST
     if (isset($_POST['dato'])) {
         // Obtener el estado del LED
         $estado = $_POST['dato'];
-        
-        // Establecer una cookie para almacenar el estado del LED
-        setcookie("estado_led", $estado, time() + (86400 * 30), "/"); // La cookie expira en 30 días
-        // Mostrar el estado del LED en la página
-        echo "<p>El estado del LED es: $estado</p>";
+    
+        // // Establecer una cookie para almacenar el estado del LED
+        // setcookie("estado_led", $estado, time() + (86400 * 30), "/"); // La cookie expira en 30 días
+        // // Mostrar el estado del LED en la página
+        // echo "<p>El estado del LED es: $estado</p>";
+
+        //Escribir datos en la memoria compartida
+        shmop_write($shmid,$estado,0);
+        echo shmop_read($shmid,0,$size_mem);
+
         
         
         
@@ -37,11 +40,18 @@
 
     } else {
         
-    // Si no se ha enviado un nuevo estado del LED, recuperar el estado de la cookie (si existe)
-    $estado = isset($_COOKIE['estado_led']) ? $_COOKIE['estado_led'] : "No hay datos anteriores";
-        // echo shmop_read($shmid,0,$size_mem);
-        echo "<p>El estado del led no ha cambiado sigue siendo: $estado .</p>";
+    // // Si no se ha enviado un nuevo estado del LED, recuperar el estado de la cookie (si existe)
+    // $estado = isset($_COOKIE['estado_led']) ? $_COOKIE['estado_led'] : "No hay datos anteriores";
+    //     // echo shmop_read($shmid,0,$size_mem);
+    //     echo "<p>El estado del led no ha cambiado sigue siendo: $estado .</p>";
+
+    // Escribimos el dato almacenado en memoria directamente
+    echo shmop_read($shmid,0,$size_mem);
+
     }
+
+
+
     ?>
     
 </body>
